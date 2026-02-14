69b037925f01fc260c74e4ebdc5453955bc2fe1e
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-02-14 17:31:28 +0900

    add(script): Add GitHub.cli to install package

--- WinSetup.ps1
+++ WinSetup.ps1
@@ -56,6 +56,7 @@ function Main-Function {
     # Install...
     
     Install-WingetPackage Git.Git
+    Install-WingetPackage GitHub.cli
     Install-WIngetPackage Python.Python.3.14
     Install-WingetPackage Microsoft.PowerShell
     Install-WingetPackage Neovim.Neovim
