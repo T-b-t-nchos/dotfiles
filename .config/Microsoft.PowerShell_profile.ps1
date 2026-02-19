9026964e51900499e0aaa6aefc9bec4f85ecd7fa
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-02-19 23:12:28 +0900

    add(pwsh): Insert NewLine when not first Prompt

--- .config/Microsoft.PowerShell_profile.ps1
+++ .config/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,15 @@ if (Test-Path $theme) {
     oh-my-posh init pwsh --config $theme | Invoke-Expression
 }
 
+
+$originalPrompt = (Get-Command prompt).ScriptBlock
+$script:__firstPrompt = $true
+
+function prompt {
+    if (-not $script:__firstPrompt) {
+        Write-Host ""
+    }
+    $script:__firstPrompt = $false
+    & $originalPrompt
+}
+
