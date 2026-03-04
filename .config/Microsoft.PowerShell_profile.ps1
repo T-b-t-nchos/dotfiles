482a37a09733fa42bb6744e3f9ca34be2c32dbcb
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-03-04 22:32:43 +0900

    feat(pwsh): Add re() function to reload enviloment

--- .config/Microsoft.PowerShell_profile.ps1
+++ .config/Microsoft.PowerShell_profile.ps1
@@ -15,3 +15,8 @@ function prompt {
         & $originalPrompt
 }
 
+function re {
+    $env:Path = 
+        [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" +
+        [System.Environment]::GetEnvironmentVariable("Path","User")
+}
