a6fba26492f7e20911a1de0341bd98fde47affa5
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-03-11 02:18:20 +0900

    fix(Neovim): Change lualine's reading timing

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-lualine/lualine.nvim',
-    event = { "BufReadPost", "BufNewFile" },
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require('lualine').setup({
