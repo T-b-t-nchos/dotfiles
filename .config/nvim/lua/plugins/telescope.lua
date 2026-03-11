410104cc20619e0be527cffbba7ff5a74991f4bd
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-03-11 18:17:14 +0900

    fix(Neovim): Add telescope's file_ignore_patterns

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,5 +7,21 @@ return {
             'nvim-telescope/telescope-fzf-native.nvim',
             build = vim.fn.executable("make") == 1 and "make" or nil,
         },
-    }
+    },
+    config = function ()
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules/.*",
+                    "%.env",
+                    "yarn.lock",
+                    "package%-lock.json",
+                    "lazy%-lock.json",
+                    "init.sql",
+                    "target/.*",
+                    ".git/.*",
+                },
+            },
+        })
+    end
 }
