c9cdbfd354a670a51457554a4a9e540446b7188a
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-03-24 21:05:07 +0900

    fix(Neovim): Change dashboard title design

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -4,21 +4,85 @@ return{
     config = function()
         vim.cmd("hi DashboardHeader gui=bold cterm=bold")
 
+        -- local function make_header()
+        --     local v = vim.version()
+        --
+        --     local header_width = vim.fn.strdisplaywidth(
+        --         "--------------------------------------- Neo - Vi IMproved"
+        --     )
+        --
+        --     local version = string.format("NVIM v%d.%d.%d", v.major, v.minor, v.patch)
+        --     local version_width = vim.fn.strdisplaywidth(version)
+        --
+        --     local padding = header_width - version_width
+        --     if padding < 0 then
+        --         padding = 0
+        --     end
+        --
+        --     local right_aligned_version = string.rep(" ", padding) .. version
+        --
+        --     return {
+        --         "",
+        --         -- "---------------------------------------------------------",
+        --         "_____   __                      _____            ",
+        --         "___  | / /__________     ___   ____(_)______ ___ ",
+        --         "__   |/ /_  _ \\  __ \\    __ | / /_  /__  __ `__ \\",
+        --         "_  /|  / /  __/ /_/ /    __ |/ /_  / _  / / / / /",
+        --         "/_/ |_/  \\___/\\____/     _____/ /_/  /_/ /_/ /_/ ",
+        --         "",
+        --         "--------------------------------------- Neo - Vi IMproved",
+        --         right_aligned_version,
+        --         "",
+        --     }
+        -- end
+
+        local function make_header()
+            local v = vim.version()
+
+            local title   = "Neo - Vi IMproved"
+            local version = string.format("NVIM v%d.%d.%d", v.major, v.minor, v.patch)
+
+            local art = {
+                "_____   __                      _____            ",
+                "___  | / /__________     ___   ____(_)______ ___ ",
+                "__   |/ /_  _ \\  __ \\    __ | / /_  /__  __ `__ \\",
+                "_  /|  / /  __/ /_/ /    __ |/ /_  / _  / / / / /",
+                "/_/ |_/  \\___/\\____/     _____/ /_/  /_/ /_/ /_/ ",
+            }
+
+            local max_width = 0
+            for _, l in ipairs(art) do
+                max_width = math.max(max_width, vim.fn.strdisplaywidth(l))
+            end
+
+            local left  = version
+            local right = title
+
+            local left_w  = vim.fn.strdisplaywidth(left)
+            local right_w = vim.fn.strdisplaywidth(right)
+
+            local dash_len = max_width - left_w - right_w - 2 + 6
+            if dash_len < 1 then
+                dash_len = 1
+            end
+
+            local middle = string.rep("-", dash_len)
+
+            local combined = string.format("%s %s %s", left, middle, right)
+
+            return vim.list_extend({
+                "",
+            }, vim.list_extend(art, {
+                "",
+                combined,
+                "",
+            }))
+        end
+
         require("dashboard").setup({
             theme = "hyper",
             config = {
-                header = {
-                    "",
-                    "---------------------------------------------------------",
-                    "_____   __                      _____            ",
-                    "___  | / /__________     ___   ____(_)______ ___ ",
-                    "__   |/ /_  _ \\  __ \\    __ | / /_  /__  __ `__ \\",
-                    "_  /|  / /  __/ /_/ /    __ |/ /_  / _  / / / / /",
-                    "/_/ |_/  \\___/\\____/     _____/ /_/  /_/ /_/ /_/ ",
-                    "",
-                    "--------------------------------------- Neo - Vi IMproved",
-                    ""
-                },
+                header = make_header(),
                 header_hl = "DashboardHeader",
 
                 --packages = { enable = false },
@@ -32,13 +96,8 @@ return{
                 mru = { enable = false },
 
                 footer = function ()
-                    local v = vim.version()
-                     return {
-                        "",
-                        string.format(
-                            " Neovim v%d.%d.%d",
-                            v.major, v.minor, v.patch
-                        ),
+                    return {
+                        ""
                     }
                 end,
             },
