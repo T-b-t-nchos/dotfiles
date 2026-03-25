6fb0c7ab6ed6187c1ee08b2aadac91cd8b47c91e
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-03-25 10:27:31 +0900

    add(Neovim): Add telescope plugin "telescope-github"

---
+++ .config/nvim/lua/plugins/telescope/telescope-github.lua
@@ -0,0 +1,8 @@
+return {
+    'nvim-telescope/telescope-github.nvim',
+    lazy = true,
+    dependencies = {
+        'nvim-telescope/telescope.nvim',
+        'nvim-lua/plenary.nvim',
+    }
+}
--- .config/nvim/lua/plugins/telescope/telescope.lua
+++ .config/nvim/lua/plugins/telescope/telescope.lua
@@ -57,5 +57,6 @@ return {
 
         require('telescope').load_extension("fzf")
         require("telescope").load_extension("file_browser")
+        require('telescope').load_extension('gh')
     end
 }
