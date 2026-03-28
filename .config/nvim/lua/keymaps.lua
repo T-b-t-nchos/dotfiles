64eb88ef3b23ff2d172305f9f14d63ac14dead5f
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-03-28 17:35:52 +0900

    add(Neovim): Add accelerted-jk.nvim and its config

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -18,6 +18,10 @@ vim.keymap.set("n", "<leader>=", function()
 end, { desc = "Reindent buffer (keep cursor)" })
 
 
+-- Move cursor
+vim.keymap.set('n', 'j', '<Plug>(accelerated_jk_gj)', {})
+vim.keymap.set('n', 'k', '<Plug>(accelerated_jk_gk)', {})
+
 -- Indent
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
