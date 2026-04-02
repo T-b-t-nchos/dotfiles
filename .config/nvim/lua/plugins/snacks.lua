5f549e01c9c6b7f0f937d121fa36172c2e53ca55
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-04-03 00:58:16 +0900

    add(Neovim): Add snacks.nvim and its config

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+    "folke/snacks.nvim",
+    event = "VeryLazy",
+    opts = {
+        bigfile = {
+            enabled = true,
+
+            notify = true,
+            size = 10 * 1024 * 1024, -- 10MB
+            line_length = 5000,
+        },
+    },
+}
