658f46cb07f63c29885d8f745f3e91b9ff9f3c5d
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-04-05 01:32:42 +0900

    add(Neovim): Add hexa.nvim as new plugin

---
+++ .config/nvim/lua/plugins/hexa.lua
@@ -0,0 +1,14 @@
+return {
+    "qleveque/hexa.nvim",
+    cmd = {
+        "HexSearch",
+        "HexSearchBack",
+        "HexReformat",
+        "HexBin",
+        "HexRun",
+        "HexGoto",
+        "Hex",
+        "UnHex",
+    },
+    opts={}
+}
