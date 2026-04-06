4816905e51b6ccaabe0269320fe42147880c4419
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-04-06 23:18:04 +0900

    fix(ohmyposh): Change main theme

--- .config/dotfiles_PowerShell_profile.ps1
+++ .config/dotfiles_PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-$theme = Join-Path $HOME ".config\ohmyposh\theme\nchos.omp.json"
+$theme = Join-Path $HOME ".config\ohmyposh\theme\Aquaposh.omp.json"
 if (Test-Path $theme) {
     oh-my-posh init pwsh --config $theme | Invoke-Expression
 }
