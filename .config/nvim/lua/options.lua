87c1ca31e19075a95655b8a1268a54b7c68d7bb3
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-04-11 15:16:22 +0900

    remove(Neovim): Remove vim.fn.stdpath()

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,9 +1,5 @@
 local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 
-vim.fn.stdpath("config")
-vim.fn.stdpath("data")
-vim.fn.stdpath("cache")
-
 if is_windows then
     vim.cmd.language("ja_JP.utf-8")
 end
