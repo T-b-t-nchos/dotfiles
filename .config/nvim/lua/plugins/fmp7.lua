232568cdac9acd1e5c939cdb298162f23fb3e794
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-04-18 01:26:56 +0900

    add(Neovim): Add FMP7.nvim as new plugin

---
+++ .config/nvim/lua/plugins/fmp7.lua
@@ -0,0 +1,11 @@
+return {
+    "T-b-t-Nchos/FMP7.nvim",
+    dev = false,
+    dir = "~/source/repos/FMP7.nvim/",
+    cmd = "FMP",
+    opts = {
+        FMP7_CLI_PATH = "C:/Users/Nchos/source/repos/FMP7.nvim/FMP7CLI/bin/Debug/FMP7CLI.exe",
+
+        fadeout_before_play = true
+    },
+}
