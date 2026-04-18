baaf8535c70ed9e3ef052ebd73e79bff1c440e2b
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-04-18 19:29:49 +0900

    refactor(Neovim): Consolidate C#-related plugins into a single file

--- .config/nvim/lua/plugins/csharp.lua
+++ .config/nvim/lua/plugins/csharp.lua
@@ -1,12 +1,28 @@
 return {
-    "iabdelkareem/csharp.nvim",
-    dependencies = {
-        "williamboman/mason.nvim", -- Required, automatically installs omnisharp
-        "mfussenegger/nvim-dap",
-        "Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
+    {
+        "iabdelkareem/csharp.nvim",
+        dependencies = {
+            "williamboman/mason.nvim", -- Required, automatically installs omnisharp
+            "mfussenegger/nvim-dap",
+            "Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
+        },
+        config = function ()
+            require("mason").setup() -- Mason setup must run before csharp, only if you want to use omnisharp
+            require("csharp").setup()
+        end
     },
-    config = function ()
-        require("mason").setup() -- Mason setup must run before csharp, only if you want to use omnisharp
-        require("csharp").setup()
-    end
+    {
+        "MonsieurTib/neonuget",
+        cmd = "Nuget",
+        ft = { "cs", "fs", "fsi", "fsscript", "fsx", "nupkg", ".csproj", ".fsproj", ".vbproj", ".sln", ".slnx" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            require("neonuget").setup({
+                dotnet_path = "dotnet",
+                default_project = nil,
+            })
+        end,
+    },
 }
