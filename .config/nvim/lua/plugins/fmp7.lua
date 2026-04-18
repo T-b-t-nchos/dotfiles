adf6f8f3414c21384123437b9669be6df194a3c4
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-04-18 19:59:55 +0900

    fix(Neovim): Fix FMP7_CLI_PATH

--- .config/nvim/lua/plugins/fmp7.lua
+++ .config/nvim/lua/plugins/fmp7.lua
@@ -4,7 +4,7 @@ return {
     dir = "~/source/repos/FMP7.nvim/",
     cmd = "FMP",
     opts = {
-        FMP7_CLI_PATH = "C:/Users/Nchos/source/repos/FMP7.nvim/FMP7CLI/bin/Debug/FMP7CLI.exe",
+        FMP7_CLI_PATH = "C:/Users/Nchos/source/repos/FMP7CLI/bin/Debug/FMP7CLI.exe",
 
         fadeout_before_play = true
     },
