cdbc27172e49d2e3a3104a97a2066a5fd0f92cfb
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-04-29 16:52:03 +0900

    fix(Neovim): Remove FMP7.nvim option "FMP7_CLI_PATH"

--- .config/nvim/lua/plugins/fmp7.lua
+++ .config/nvim/lua/plugins/fmp7.lua
@@ -3,8 +3,6 @@ return {
     dev = false,
     cmd = "FMP",
     opts = {
-        FMP7_CLI_PATH = "C:/Users/Nchos/source/repos/FMP7CLI/bin/Debug/FMP7CLI.exe",
-
         fmp7_path = "N:/FMPandPMD/FMP7.exe",
         fadeout_before_play = true,
         play_roots = {
