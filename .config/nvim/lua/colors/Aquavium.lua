1b1336141f085853e944843d2d8321d957a09abf
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-05-30 16:02:40 +0900

    fix(Neovim): Remove dynamic transparent changing feature

--- .config/nvim/lua/colors/Aquavium.lua
+++ .config/nvim/lua/colors/Aquavium.lua
@@ -6,24 +6,24 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
-            local temp_dir = vim.env.TEMP or vim.env.TMP or vim.env.TEMPDIR or "/tmp"
-
-            local sep = package.config:sub(1, 1)
-            local opacity_file = temp_dir .. sep .. "wezterm_opacity.tmp"
-
-            local opacity = nil
-            local file = io.open(opacity_file, "r")
-
-            if file then
-                local content = file:read("*a")
-                file:close()
-                opacity = tonumber(content)
-            end
+            -- local temp_dir = vim.env.TEMP or vim.env.TMP or vim.env.TEMPDIR or "/tmp"
+            --
+            -- local sep = package.config:sub(1, 1)
+            -- local opacity_file = temp_dir .. sep .. "wezterm_opacity.tmp"
+            --
+            -- local opacity = nil
+            -- local file = io.open(opacity_file, "r")
+            --
+            -- if file then
+            --     local content = file:read("*a")
+            --     file:close()
+            --     opacity = tonumber(content)
+            -- end
 
             local aquavium = require("Aquavium")
 
             aquavium.setup({
-                transparent = opacity == nil or opacity ~= 1.0,
+                -- transparent = opacity == nil or opacity ~= 1.0,
             })
 
             if not vim.g.colors_name then
