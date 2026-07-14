f3f64caca4174ce919d08b221f5fbbc3d053ac8e
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-07-14 18:36:16 +0900

    feat: Add mkcd custom command

--- .config/dotfiles_PowerShell_profile.ps1
+++ .config/dotfiles_PowerShell_profile.ps1
@@ -3,9 +3,17 @@ if (Test-Path $theme) {
     oh-my-posh init pwsh --config $theme | Invoke-Expression
 }
 
-
+# Custom commands
 function re {
     $env:Path = 
         [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" +
         [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+
+function mkcd {
+    param([string]$path)
+    if (-not (Test-Path $path)) {
+        New-Item -ItemType Directory -Force -Path $path
+    }
+    Set-Location $path
 }
