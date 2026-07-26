433bec23277d07e663d199b3106dff283f4e3da6
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-07-26 22:32:08 +0900

    remove(Neovim): disable SayC8/license_gen.nvim due to the removed repo

--- .config/nvim/lua/plugins/license_gen.lua
+++ .config/nvim/lua/plugins/license_gen.lua
@@ -1,8 +1,8 @@
 return {
-    "SayC8/license_gen.nvim",
-    cmd = "AddLicense",
-    config = function()
-        require("license_gen").setup({
-        })
-    end
+    -- "SayC8/license_gen.nvim",
+    -- cmd = "AddLicense",
+    -- config = function()
+    --     require("license_gen").setup({
+    --     })
+    -- end
 }
