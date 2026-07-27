a631714893ddefadfc8ff8155238172bfafcb2f5
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-07-27 23:05:39 +0900

    fix(Neovim): change update check freq to once in a week

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
     },
     checker = {
         enabled = true,
-        frequency = 86400,
+        frequency = 1 * 60 * 60 * 24 * 7,
     },
     performance = {
         cache = {
