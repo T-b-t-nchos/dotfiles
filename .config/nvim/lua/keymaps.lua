5d78d812fb0296c7cc1aa17401f49611caf8ecd2
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-07-30 18:36:47 +0900

    feat(Neovim)!: add/change/delete some keymaps like lazyvim

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -27,6 +27,51 @@ vks('n', 'k', '<Plug>(accelerated_jk_gk)', {})
 vks("v", "<", "<gv")
 vks("v", ">", ">gv")
 
+-- buffers
+vks("n", "H", "<cmd>bprev<CR>", { silent = true, desc = "Move Buffer Prev(←)"})
+vks("n", "L", "<cmd>bnext<CR>", { silent = true, desc = "Move Buffer Next(→)"})
+vks("n", "<leader>bb", "<cmd>e #<cr>", { silent = true, desc = "Switch to other buffer" })
+vks("n", "<leader>`", "<cmd>e #<cr>", { silent = true, desc = "Switch to other buffer" })
+vks("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+
+-- Tab
+vks("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+vks("n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Close Other Tabs" })
+vks("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+vks("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+vks("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+vks("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+vks("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Prev Tab" })
+
+-- Window
+vks("n", "<leader>|", "<cmd>vsplit<cr>", { silent = true, desc = "vsprit" })
+vks("n", "<leader>-", "<cmd>split<cr>", { silent = true, desc = "hsprit" })
+vks("n", "<leader>wd", "<C-W>c", { silent = true, desc = "close window" })
+
+-- pane move prefix: Ctrl + hjkl
+vks("n", "<C-h>", "<C-w>h",  { desc = "Pane left" })
+vks("n", "<C-l>", "<C-w>l",  { desc = "Pane right" })
+vks("n", "<C-k>", "<C-w>k", { desc = "Pane up" })
+vks("n", "<C-j>", "<C-w>j", { desc = "Pane down" })
+vks("n", "<C-A-h>", "<C-w>H",  { desc = "Pane left" })
+vks("n", "<C-A-l>", "<C-w>L",  { desc = "Pane right" })
+vks("n", "<C-A-k>", "<C-w>K", { desc = "Pane up" })
+vks("n", "<C-A-j>", "<C-w>J", { desc = "Pane down" })
+
+-- resize pane
+vks("n", "<C-Up>", "<cmd>resize +1<CR>", { silent = true })
+vks("n", "<C-Down>", "<cmd>resize -1<CR>", { silent = true })
+vks("n", "<C-Left>", "<cmd>vertical resize -1<CR>", { silent = true })
+vks("n", "<C-Right>", "<cmd>vertical resize +1<CR>", { silent = true })
+
+-- Escape and Clear hlsearch
+vks({ "i", "n", "s" }, "<esc>", function()
+    vim.cmd("noh")
+    return "<esc>"
+end, { expr = true, desc = "Escape and Clear hlsearch" })
+
+
+
 -- Telescope
 vks("n", "<leader>Tff", "<cmd>Telescope find_files<cr>", { silent = true, desc = "Find File" })
 vks("n", "<leader>Tfb", "<cwd>Telescope file_browser<cr>", { silent = true, desc = "File Browser" })
@@ -39,17 +84,6 @@ vks("n", "<leader>Th", "<cmd>Telescope help_tags<cr>", { silent = true, desc = "
 vks("n", "<leader>Tk", "<cmd>Telescope keymaps<cr>", { silent = true, desc = "Keymaps" })
 vks("n", "<leader>Tv", "<cmd>Telescope vim_options<cr>", { silent = true, desc = "Vim Options" })
 vks("n", "<leader>Tr", "<cmd>Telescope registers<cr>", { silent = true, desc = "Registers" })
-
--- buffers
-vks("n", "<leader>bj", "<cmd>bnext<CR>", { silent = true, desc = "Move Buffer Next(→)"})
-vks("n", "<leader>bk", "<cmd>bprev<CR>", { silent = true, desc = "Move Buffer Prev(←)"})
-vks("n", "<leader>b<Left>", "<cmd>bprev<CR>", { silent = true, desc = "Move Buffer Prev(←)"})
-vks("n", "<leader>b<Right>", "<cmd>bnext<CR>", { silent = true, desc = "Move Buffer Next(→)"})
-vks("n", "<leader>bh", "<cmd>bprev<CR>", { silent = true, desc = "Move Buffer Prev(←)"})
-vks("n", "<leader>bl", "<cmd>bnext<CR>", { silent = true, desc = "Move Buffer Next(→)"})
-vks("n", "H", "<cmd>bprev<CR>", { silent = true, desc = "Move Buffer Prev(←)"})
-vks("n", "L", "<cmd>bnext<CR>", { silent = true, desc = "Move Buffer Next(→)"})
-vks("n", "<leader>X", "<cmd>bd<CR>", { silent = true, desc = "Delete buffer" })
 
 
 -- neotree
@@ -69,28 +103,6 @@ vks("n", "<leader>l", "<cmd>AerialToggle<CR>", { desc = "Outline Window" })
 vks("n", "<C-{>", "<cmd>AerialPrev<CR>")
 vks("n", "<C-}>", "<cmd>AerialNext<CR>")
 
-
--- Window
-vks("n", "<leader>|", "<cmd>vsplit<cr>", { silent = true, desc = "vsprit" })
-vks("n", "<leader>-", "<cmd>split<cr>", { silent = true, desc = "hsprit" })
-vks("n", "<leader>wd", "<C-W>c", { silent = true, desc = "close window" })
-
-
--- pane move prefix: Ctrl + hjkl
-vks("n", "<C-h>", "<C-w>h",  { desc = "Pane left" })
-vks("n", "<C-l>", "<C-w>l",  { desc = "Pane right" })
-vks("n", "<C-k>", "<C-w>k", { desc = "Pane up" })
-vks("n", "<C-j>", "<C-w>j", { desc = "Pane down" })
-vks("n", "<C-A-h>", "<C-w>H",  { desc = "Pane left" })
-vks("n", "<C-A-l>", "<C-w>L",  { desc = "Pane right" })
-vks("n", "<C-A-k>", "<C-w>K", { desc = "Pane up" })
-vks("n", "<C-A-j>", "<C-w>J", { desc = "Pane down" })
-
--- resize pane
-vks("n", "<C-Up>", "<cmd>resize +1<CR>", { silent = true })
-vks("n", "<C-Down>", "<cmd>resize -1<CR>", { silent = true })
-vks("n", "<C-Left>", "<cmd>vertical resize -1<CR>", { silent = true })
-vks("n", "<C-Right>", "<cmd>vertical resize +1<CR>", { silent = true })
 
 -- vks("n", "<leader>wa",  "<C-w>h", { desc = "Window left" })
 -- vks("n", "<leader>wd", "<C-w>l", { desc = "Window right" })
