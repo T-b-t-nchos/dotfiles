1ef2aad426e00cf0b0cb5b1913e0acfe311fbf62
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-07-31 23:31:05 +0900

    fix(Neovim/Tex): fix vimtex option

--- .config/nvim/lua/plugins/TEX.lua
+++ .config/nvim/lua/plugins/TEX.lua
@@ -18,13 +18,10 @@ return {
                 },
             }
 
-            vim.g.vimtex_view_general_options =
-            "-r @line @pdf @tex"
-
-            if vim.fn.has('win32') == 1 or (vim.fn.has('unix') == 1 and vim.fn.exists('$WSLENV') == 1) then
+            if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 or (vim.fn.has('unix') == 1 and vim.fn.exists('$WSLENV') == 1) then
                 if vim.fn.executable('SumatraPDF.exe') == 1 then
-                    vim.g.vimtex_view_general_viewer = 'SumatraPDF.exe'
-                    vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+                    vim.g.vimtex_view_general_viewer = vim.fn.expand("$LOCALAPPDATA") .. "/SumatraPDF/SumatraPDF.exe"
+                    vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
                 end
             end
         end,
