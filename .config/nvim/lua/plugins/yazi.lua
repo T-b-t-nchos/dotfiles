85d859117a3a138c0bd799b35d80034029c73001
T-b-t-nchos <T-b-t-nchos@outlook.com>
2026-08-06 21:36:18 +0900

    fix(Neovim): fix load timing of yazi.nvim

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -1,7 +1,8 @@
 return {
     "mikavilpas/yazi.nvim",
     version = "*",
-    event = "VeryLazy",
+    -- event = "VeryLazy",
+    cmd = { "Yazi" },
     keys = {
         { "<leader>yz", "<cmd>Yazi<cr>", desc = "Yazi" },
         { "<leader>yZ", "<cmd>Yazi cwd<cr>", desc = "Yazi in nvim's working directory" },
